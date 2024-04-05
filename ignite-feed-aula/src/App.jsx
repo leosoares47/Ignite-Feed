f193ad7aaa91c521f9507ee959371a500f1f76b5
leosoares47 <leochubby@hotmail.com>
2024-04-05 15:49:11 -0300

    antes do gpt

--- ignite-feed-aula/src/App.jsx
+++ ignite-feed-aula/src/App.jsx
@@ -7,6 +7,16 @@ export function App() {
         <img src="src/assets/logo.png" alt="Logotipo Ignite Feed" />
         <h1>Ignite Feed</h1>
       </header>
+      <section className="sectionp">
+        <img src="src/assets/perfil.png" alt="foto de perfil" />
+        <h3>Leslie Alexander</h3>
+        <p>UI Designer</p>
+        <div className="separator"></div>
+        <button className="editperfil">
+          <img src="src/assets/pincel.png" alt="pincel" />
+          Editar seu Perfil
+        </button>
+      </section>
     </div>
   )
 }
