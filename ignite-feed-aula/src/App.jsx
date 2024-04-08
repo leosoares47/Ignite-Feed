e7944f9377fbfef05496b258d11a49fa512273a4
leosoares47 <leochubby@hotmail.com>
2024-04-08 17:16:18 -0300

    header

--- ignite-feed-aula/src/App.jsx
+++ ignite-feed-aula/src/App.jsx
@@ -1,22 +1,20 @@
-import React from "react"
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+import "./global.css"
 
 export function App() {
   return (
     <div>
-      <header className="header">
-        <img src="src/assets/logo.png" alt="Logotipo Ignite Feed" />
-        <h1>Ignite Feed</h1>
-      </header>
-      <section className="sectionp">
-        <img src="src/assets/perfil.png" alt="foto de perfil" />
-        <h3>Leslie Alexander</h3>
-        <p>UI Designer</p>
-        <div className="separator"></div>
-        <button className="editperfil">
-          <img src="src/assets/pincel.png" alt="pincel" />
-          Editar seu Perfil
-        </button>
-      </section>
+      <Header />
+
+      <Post
+        author="Leonardo Soares"
+        content="TesteTesteTesteTesteTesteTesteTeste"
+      />
+      <Post
+        author="Lorena Soares"
+        content="TesteTesteTesteTesteTesteTesteTesteTeste"
+      />
     </div>
   )
 }
